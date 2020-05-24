4cab42285edf0bd15cf073ac5b3373b942cdd978
Nie Yuansheng <mail2nys@163.com>
2020-05-24 09:23:34 +0800

    API信息业务层测试类

---
+++ src/test/java/com/nys/apilist/service/impl/ApiInfoServiceImplTest.java
@@ -0,0 +1,39 @@
+package com.nys.apilist.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.apache.http.util.Asserts;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.nys.apilist.dataobject.ApiInfo;
+
+/**
+ * API信息Service实现类的测试
+ * @author nysheng
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class ApiInfoServiceImplTest {
+
+	@Autowired
+	private ApiInfoServiceImpl apiInfoServiceImpl;
+	@Test
+	void testGetAllAPI() {
+		 List<ApiInfo> results= apiInfoServiceImpl.getAllAPI();
+		 Assert.assertNotNull(results);
+	}
+
+	@Test
+	void testGetAPIById() {
+		ApiInfo apiInfo=apiInfoServiceImpl.getAPIById(10000);
+		Assert.assertNotNull(apiInfo);
+	}
+
+}
