6c126f69bb952957f9e3ea58b62c4d9b4321449e
Nie Yuansheng <mail2nys@163.com>
2020-05-24 10:55:43 +0800

    添加toString()方法

--- src/main/java/com/nys/apilist/dataobject/ApiInfo.java
+++ src/main/java/com/nys/apilist/dataobject/ApiInfo.java
@@ -2,6 +2,7 @@ package com.nys.apilist.dataobject;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Data;
@@ -15,7 +16,7 @@ import lombok.Data;
 @Data
 public class ApiInfo {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String url;
@@ -23,4 +24,11 @@ public class ApiInfo {
 	private String name;
 	private String image;
 	private String comment;
+	@Override
+	public String toString() {
+		return "ApiInfo [id=" + id + ", url=" + url + ", paramsStr=" + paramsStr + ", name=" + name + ", image=" + image
+				+ ", comment=" + comment + "]";
+	}
+	
+	
 }
