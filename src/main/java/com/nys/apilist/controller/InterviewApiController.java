268965269b6eeb3397bf0a1d8dc0791a78dabf44
Nie Yuansheng <mail2nys@163.com>
2020-05-24 20:21:28 +0800

    添加参数位于url上的有参请求方式

--- src/main/java/com/nys/apilist/controller/InterviewApiController.java
+++ src/main/java/com/nys/apilist/controller/InterviewApiController.java
@@ -3,24 +3,15 @@ package com.nys.apilist.controller;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.google.gson.JsonObject;
 import com.nys.apilist.dataobject.ApiInfo;
 import com.nys.apilist.service.ApiInfoService;
-import com.nys.apilist.vo.ResultVO;
-
-import freemarker.core.JSONOutputFormat;
 
 
 
@@ -39,7 +30,7 @@ public class InterviewApiController {
 	 * @return
 	 */
 	@GetMapping("interview/{id}")
-	public JSONObject interview(@PathVariable("id")Integer id,Map<String, Object> map,HttpServletRequest request){
+	public Object interview(@PathVariable("id")Integer id,Map<String, Object> map,HttpServletRequest request){
 		//去数据库查询API详情
 		ApiInfo apiInfo=apiInfoService.getAPIById(id);
 		if(apiInfo==null) {
@@ -56,8 +47,37 @@ public class InterviewApiController {
 		param=param.substring(1);
 		//发送get请求
 		RestTemplate restTemplate=new RestTemplate();
-		JSONObject result=restTemplate.getForObject(apiInfo.getUrl()+"?"+param, JSONObject.class);
-		return result;
+		String result=restTemplate.getForObject(apiInfo.getUrl()+"?"+param, String.class);
+		JSONObject jo=new JSONObject();
+		return jo.parse(result);
+	}
+	/**
+	 * 发送有参数的get请求-以url参数的形式
+	 * @param id
+	 * @param map
+	 * @param request
+	 * @return
+	 */
+	@GetMapping("interview_path/{id}")
+	public Object interview_path(@PathVariable("id")Integer id,Map<String, Object> map,HttpServletRequest request){
+		//去数据库查询API详情
+		ApiInfo apiInfo=apiInfoService.getAPIById(id);
+		if(apiInfo==null) {
+			//TODO...
+		}
+		String paramStr=apiInfo.getParamsStr();
+		//解析参数
+		String[] params=paramStr.split("-");
+		String param="";
+		for(String key : params) {
+			String value=request.getParameter(key);
+			param+="/"+key+"/"+value;
+		}
+		//发送get请求
+		RestTemplate restTemplate=new RestTemplate();
+		String result=restTemplate.getForObject(apiInfo.getUrl()+param, String.class);
+		JSONObject jo=new JSONObject();
+		return jo.parse(result);
 	}
 	/**
 	 * 发送无参get请求
