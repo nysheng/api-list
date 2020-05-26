a5581302eb11fdbf0ed2cc4e26ff8a8c20230a39
Nie Yuansheng <mail2nys@163.com>
2020-05-26 18:49:32 +0800

    删除多余的依赖包

--- src/main/java/com/nys/apilist/controller/IndexController.java
+++ src/main/java/com/nys/apilist/controller/IndexController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.nys.apilist.dataobject.ApiInfo;
@@ -21,12 +20,11 @@ public class IndexController {
 	private ApiInfoService apiInfoService;
 	/**
 	 * 主页数据
-	 * @param openid 微信openid
 	 * @param map
 	 * @return
 	 */
 	@GetMapping("/index")
-	public ModelAndView index(@RequestParam("openid")String openid,Map<String, Object> map) {
+	public ModelAndView index(Map<String, Object> map) {
 		//1.openid校验
 		//TODO...
 		//2.获取所有API信息
--- src/main/java/com/nys/apilist/controller/InterviewApiController.java
+++ src/main/java/com/nys/apilist/controller/InterviewApiController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -71,7 +72,11 @@ public class InterviewApiController {
 		String param="";
 		for(String key : params) {
 			String value=request.getParameter(key);
-			param+="/"+key+"/"+value;
+			if(StringUtils.isEmpty(value)) {
+				param+="/"+key;
+			}else {
+				param+="/"+key+"/"+value;
+			}
 		}
 		//发送get请求
 		RestTemplate restTemplate=new RestTemplate();
